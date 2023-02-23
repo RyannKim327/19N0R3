064dc3a7c29c2ce8143490919343b1601df07e63
RyannKim327 <rksesgundo123@gmail.com>
2023-02-23 13:26:50 +0800

    02-23-23 13-26 [Changed Design for desktop]

--- src/script.js
+++ src/script.js
@@ -9,6 +9,8 @@ const get = async (url) => {
 }
 
 let total = 0
+let max = 25
+let id = -1
 let _s = () => {
 	total = 0
 	filter()
@@ -24,16 +26,16 @@ let filter = async () => {
 	for(let x = 0; x < list.length; x++){
 	let e = list[x]
 		if(s == ""){
-			if(total < 10){
-				t += `<li><a href="read?id=${e.id}">${e.title}</a></li>`
+			if(total < max){
+				t += `<li onclick="read('${e.id}')">${e.title}</a></li>`
 				total++
 			}
 		}else{
 			let f = e.title.toLowerCase()
 			let g = s.toLowerCase()
 			if(f.includes(g)){
-				if(total < 10){
-					t += `<li><a href="read/?id=${e.id}">${e.title}</a></li>`
+				if(total < max){
+					t += `<li onclick="read('${e.id}')">${e.title}</a></li>`
 					total++
 				}
 			}
@@ -42,6 +44,24 @@ let filter = async () => {
 	lists.innerHTML = t
 }
 
+async function read(id_num){
+	let title = document.getElementById("poem-title")
+	let content = document.getElementById("poem-content")
+	let poem = await fetch(`/read/${id_num}`).then(r => {
+		return r.json()
+	}).catch(error => {
+		console.error(`Error [Read]: ${e}`)
+		return null
+	})
+	title.textContent = poem.title
+	console.log(poem.content)
+	content.innerHTML = poem.content.replace(/\r\n/gi, "<br>").replace(/\n/gi, "<br>").replace(/\\/gi, "\\")
+}
+
 window.onload = () => {
+	_s()
+}
+let search = document.getElementById("search")
+search.onkeyup = () => {
 	_s()
 }
