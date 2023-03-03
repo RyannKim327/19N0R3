380790dc8f23c1790bd1e756aacb384a25bad97e
RyannKim327 <rksesgundo123@gmail.com>
2023-03-04 01:52:01 +0800

    03-04-23 01:51 [Fixing mobile upload]

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get(write, (req, res) => {
 app.post(write, body, (req, res) => {
 	const title = req.body.title
 	const author = req.body.author || "Unknown Author"
-	const content = req.body.content.replace(/\-([\w]+)\-\n/gi, "").replace(/\r/gi, "")
+	const content = req.body.content.replace(/\r/gi, "")
 	if(title == undefined || content == undefined || title == "" || content == "")
 		return res.send("No Result")
 	let json = JSON.parse(fs.readFileSync("data.json", "utf8"))
