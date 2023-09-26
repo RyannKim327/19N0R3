3fa9a595c02038d45b3d1893f42c1ffd0a67a687
RyannKim327 <mpoprev2@email.com>
2023-09-26 21:55:11 +0800

    9/26/2023 9:55:11 PM [Autogit]

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,8 @@
 from flask import jsonify
 from main import app
-from database import database
+from database.db import database
 
 @app.route("/api/getAllPoems")
 def getAllPoems():
     db = database()
+    db
