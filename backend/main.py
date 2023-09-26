cd3377eb1b0d684e89972c5269bd831694437fa1
RyannKim327 <mpoprev2@email.com>
2023-09-26 21:51:50 +0800

    9/26/2023 9:51:50 PM [Autogit]

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify, render_template
-
+from api import api
 app = Flask(__name__, static_url_path="/static")
 
 @app.route("/")
 def index():
 	return render_template("a.html")
-
-
-# API
