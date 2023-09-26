dae88aea4cfed7aee34c379805a18f648da91774
RyannKim327 <mpoprev2@email.com>
2023-09-26 21:41:06 +0800

    9/26/2023 9:41:06 PM [Autogit]

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,3 +1,22 @@
 window.onload = () => {
-    // document.getElementById("loading").style.display = "none"
+	document.getElementById("loading").style.display = "none"
 }
+
+const title = document.getElementById("title")
+const content = document.getElementById("content")
+
+let stored_data = ""
+
+setInterval(() => {
+	fetch("/api/getAllPoems").then(r = > {
+		return r.json()
+	}).then((r) => {
+		let new_data = JSON.stringify(r)
+		if(stored_data != new_data){
+			stored_data = new_data
+
+			// Update List
+			
+		}
+	})
+})
