73b977dd559821a1240f0c4c940a66dce2d4d6fe
RyannKim327 <mpoprev2@email.com>
2023-09-27 22:06:55 +0800

    9/27/2023 10:06:55 PM [Autogit]

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ def getAllPoems():
 	data = db.query("SELECT * FROM poems ORDER BY ID DESC")
 	result = []
 	for i in data.fetchall():
-		user = db.query(f"SELECT * FROM users WHERE ID = {i[3]}").fetchall()[0]
+		user = db.query(f"SELECT * FROM users WHERE ID = {i[3]}").fetchone()
 		result.append({
 			"id": i[0],
 			"title": i[1],
