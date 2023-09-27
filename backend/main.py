f842db945c9e8c9ec1d4350d4eb6811ba3c06765
RyannKim327 <mpoprev2@email.com>
2023-09-27 22:06:15 +0800

    9/27/2023 10:06:15 PM [Autogit]

--- backend/main.py
+++ backend/main.py
@@ -15,12 +15,12 @@ def getAllPoems():
 	data = db.query("SELECT * FROM poems ORDER BY ID DESC")
 	result = []
 	for i in data.fetchall():
-		user = db.query(f"SELECT * FROM users WHERE ID = {i[3]}").fetchall()
+		user = db.query(f"SELECT * FROM users WHERE ID = {i[3]}").fetchall()[0]
 		result.append({
 			"id": i[0],
 			"title": i[1],
 			"content": i[2],
-			"author": user[0]
+			"author": user[1]
 		})
 	return jsonify({
 		"status": 200,
