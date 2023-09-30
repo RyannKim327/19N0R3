d91cd01e08b21a1e05563832bee5590ddccb86da
RyannKim327 <mpoprev2@email.com>
2023-09-30 13:46:29 +0800

    9/30/2023 1:46:29 PM [Autogit]

--- backend/main.py
+++ backend/main.py
@@ -64,7 +64,7 @@ def credentials():
 	data = json.loads(request.data)
 	db = database()
 	print(data['password1'])
-	username = data['username'].replace("'", "\'").replace('"', '\"').replace("-", "\-")
+	username = data['username'].replace("'", "\'").replace('"', '\"').replace("--", "- -")
 	password = encrypt(data['password'])
 	if data['password1'] == "":
 		x = db.query(f"SELECT * FROM users WHERE penname = '{username}' AND password = '{password}' COLLATE NOCASE").fetchone()
