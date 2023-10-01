934a1078f5328559913386b93cc6fd9e52e6c412
RyannKim327 <mpoprev2@email.com>
2023-10-01 09:14:12 +0800

    10/1/2023 9:14:11 AM [Autogit]

--- names.py
+++ names.py
@@ -31,6 +31,8 @@ while(y < 60){
     f1 = random.randint(0, len(f))
     l1 = random.randint(0, len(l))
     n = f1 + " " l1
-    if n 
-    y += 1
+    if not n in x:
+        x.append(n)
+        print()
+        y += 1
 }
