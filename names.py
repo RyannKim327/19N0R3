b68bd44fa3dcce80b2afedf5a441b4f034f1ce30
RyannKim327 <mpoprev2@email.com>
2023-10-01 09:18:33 +0800

    10/1/2023 9:18:33 AM [Autogit]

--- names.py
+++ names.py
@@ -28,8 +28,8 @@ l = [
 x = []
 y = 1
 while(y < 60):
-    f1 = f[random.randint(0, len(f))]
-    l1 = lrandom.randint(0, len(l))
+    f1 = f[random.randint(0, len(f) - 1)]
+    l1 = l[random.randint(0, len(l) - 1)]
     n = f1 + " " + l1
     if not n in x:
         x.append(n)
