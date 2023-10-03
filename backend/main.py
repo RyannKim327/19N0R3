7982ce4b94f0e51c0904fa1e3c3437867ba28263
RyannKim327 <rksesgundo123@gmail.com>
2023-10-03 09:37:10 +0800

    10/3/2023 9:37:10 AM [Autogit]

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,7 @@ def login():
 
 @app.route("/api/get-all-poems")
 def getAllPoems():
+	if request.query_string.n
 	db = database()
 	data = db.query("SELECT * FROM poems ORDER BY ID DESC")
 	result = []
