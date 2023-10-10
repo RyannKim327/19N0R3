ac06648f6e0be7aa1f605297bd0f6accd7e10ae8
RyannKim327 <rksesgundo123@gmail.com>
2023-10-10 19:56:12 +0800

    10/10/2023 7:56:11 PM [Autogit]

--- backend/database/a.py
+++ backend/database/a.py
@@ -4,5 +4,5 @@ db = database()
 
 q = "19N0R3"
 
-a = db.query(f"SELECT title FROM poems WHERE title LIKE '%{q}%' or content LIKE '%{1}%' UNION SELECT penname FROM users WHERE penname LIKE '%{q}%'")
+a = db.query(f"SELECT title, content FROM poems WHERE title LIKE '%{q}%' or content LIKE '%{1}%' UNION SELECT penname, penname as user FROM users WHERE penname LIKE '%{q}%'")
 print(a.fetchall())
