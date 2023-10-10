4e09cc6232831a37eb66b34a03e2e5783106119c
RyannKim327 <rksesgundo123@gmail.com>
2023-10-10 19:47:27 +0800

    10/10/2023 7:47:27 PM [Autogit]

--- a.py
+++ a.py
@@ -2,5 +2,5 @@ from backend.database.db import database
 
 db = database()
 
-a = db.query("")
+a = db.query("SELECT * FROM poems WHERE ID = 1 UNION SELECT * FROM users WHERE penname LIKE '%19N0R3%'")
 print(a)
