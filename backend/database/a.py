90970f1fe977463a4d5a9736f466242ecf23639f
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 06:16:47 +0800

    10/11/2023 6:16:47 AM [Autogit]

--- backend/database/a.py
+++ backend/database/a.py
@@ -2,7 +2,7 @@ from db import database
 
 db = database()
 
-q = "sana"
+q = "19N0R3"
 
-a = db.query(f"SELECT * FROM poems RIGHT JOIN users ON poems.author = users.ID WHERE poems.title LIKE '%{q}%' OR poems.content LIKE '%{q}%' OR users.penname LIKE '%{q}%'")
+a = db.query(f"SELECT * FROM poems RIGHT JOIN users ON poems.author = users.ID WHERE poems.title LIKE '%{q}%' OR poems.content LIKE '%{q}%' OR users.penname LIKE '%{q}%' ORDER BY poems.ID DESC LIMIT 5")
 print(a.fetchall())
