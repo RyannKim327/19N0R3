8327cfff522253b284c5c9c775af43c7cc733401
RyannKim327 <rksesgundo123@gmail.com>
2023-10-11 09:17:57 +0800

    10/11/2023 9:17:57 AM [Autogit]

--- backend/main.py
+++ backend/main.py
@@ -61,7 +61,7 @@ def getAllPoems():
 		data = db.query(f"SELECT * FROM poems RIGHT JOIN users ON poems.author = users.ID ORDER BY ID DESC LIMIT {params}, {limit}")
 	else:
 		q = request.args.get("q").replace("-", "").replace("'", "").replace('"', '')
-		data = db.query(f"SELECT * FROM poems RIGHT JOIN users ON poems.author = users.ID WHERE poems.title LIKE '%{q}%' OR poems.content LIKE '%{q}%' ORDER BY ID DESC LIMIT {params}, {limit}")
+		data = db.query(f"SELECT * FROM poems RIGHT JOIN users ON poems.author = users.ID WHERE poems.title LIKE '%{q}%' OR poems.content LIKE '%{q}%' OR users.penname LIKE '%{q}%' ORDER BY ID DESC LIMIT {params}, {limit}")
 	result = []
 	for i in data.fetchall():
 		user = db.query(f"SELECT * FROM users WHERE ID = {i[3]}").fetchall()[0]
