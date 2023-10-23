c1f17b7a09a9c76bd4c45450fa4b238d19cd01af
RyannKim327 <rksesgundo123@gmail.com>
2023-10-23 19:08:23 +0800

    10/23/2023 7:08:23 PM [Autogit]

--- backend/hash.py
+++ backend/hash.py
@@ -1,30 +1,30 @@
 class SolarSystem:
-	
-alpha = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz "
-keyword = "MPOP Reverse II"
-key = []
-for i in range(len(keyword)):
-	for j in range(len(alpha)):
-		if alpha[j] == keyword[i]:
-			key.append(j + 1)
-			break
+	def __init__(self):
+		alpha = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz "
+		keyword = "MPOP Reverse II"
+		self.key = []
+		for i in range(len(keyword)):
+			for j in range(len(alpha)):
+				if alpha[j] == keyword[i]:
+					self.key.append(j + 1)
+					break
 
-def encrypt(key, text):
-	_ = ""
-	x = 0
-	for i in text:
-		_ += chr(ord(i) + key[x])
-		x += 1
-		if x >= len(key):
-			x = 0
-	return _
+	def encrypt(self, text):
+		_ = ""
+		x = 0
+		for i in text:
+			_ += chr(ord(i) + self.key[x])
+			x += 1
+			if x >= len(self.key):
+				x = 0
+		return _
 
-def decrypt(key, text):
-	_ = ""
-	x = 0
-	for i in text:
-		_ += chr(ord(i) - key[x])
-		x += 1
-		if x >= len(key):
-			x = 0
-	return _
+	def decrypt(self, text):
+		_ = ""
+		x = 0
+		for i in text:
+			_ += chr(ord(i) - self.key[x])
+			x += 1
+			if x >= len(self.key):
+				x = 0
+		return _
