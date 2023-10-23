20e37c49fced302d4c7a3b92e4601c1dc848c60d
RyannKim327 <rksesgundo123@gmail.com>
2023-10-23 19:23:43 +0800

    10/23/2023 7:23:43 PM [Autogit]

--- backend/database/db.py
+++ backend/database/db.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class database:
 	def __init__(self):
-		self.dbase = sqlite3.connect("backend/database/b.sqlite", check_same_thread=False)
+		self.dbase = sqlite3.connect("backend/database/a.sqlite", check_same_thread=False)
 		self.cur = self.dbase.cursor()
 		self.cur.execute("""
 			CREATE TABLE IF NOT EXISTS poems (
