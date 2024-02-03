7a30ac9fa3230c5e30193960ca1b320c32e47cc7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 09:57:40 +0800

    02-03-24 09:57 [Added Fixed Minor Credentials]

--- backend/main.py
+++ backend/main.py
@@ -175,4 +175,5 @@ def sendToAdmin():
 	})
 
 if __name__ == "__main__":
+	app.debug = True
 	app.run(host="0.0.0.0", port=5000)
--- backend/static/form.js
+++ backend/static/form.js
@@ -1,44 +1,151 @@
-function setCookie(key, value){
-	const date = new Date()
-	date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000))
-	let xp = `expires=${date.toUTCString()}`
-	document.cookie = `${key}=${value};${xp};path=/`
+function download(){
+	location.href = "/download"
 }
 
-// document.getElementById("username").onkeyup = (event) => {
-// 	fetch("/api/confirm-user", {
-// 		method: "POST",
-// 		body: JSON.stringify({
-// 			"username": document.getElementById("username").value
-// 		})
-// 	}).then(response => {
-// 		return response.json()
-// 	}).then(response => {
-// 		if(response.total){
-// 			alert("Meron")
-// 		}else{
-// 			alert("wala")
-// 		}
-// 	}).catch(error => {
-// 		console.error(`Error: ${error}`)
-// 	})
-// }
+function sharelink(){
+	navigator.clipboard.writeText(`Here's a like of a poem written by ${share['author']} entitled ${share['title']}\n\nYou may read it at https://pen-and-love.writers.repl.co/read/${share['ID']}`)
+	alert("The content is already in your clipboard.")
+}
 
-document.getElementById("proceed").onclick = () => {
-	fetch("/api/credentials", {
-		method: "POST",
-		body: JSON.stringify({
-			"username": document.getElementById("username").value,
-			"password": document.getElementById("password").value,
-			"password1": document.getElementById("password1").value
+function about(){
+	document.getElementById("poem-contents").style.display = "block"
+	document.getElementById("formlogin").style.display = "none"
+	document.getElementById("title").textContent = "About"
+	document.getElementById("author").textContent = ""
+	document.getElementById("content").innerHTML = "<blockquote>The \"Pen and Love\" is a platform where we share the thoughts and ideas, as well as the emotions and love to one another.</blockquote>"
+}
+
+function home(){
+	document.getElementById("poem-contents").style.display = "block"
+	document.getElementById("formlogin").style.display = "none"
+	let cookie_data = getCookie("poemID").replace(/=/gi, "")
+	poem(cookie_data)
+}
+
+function credentials(){
+	const base = document.createElement("div")
+	const form = document.createElement("div")
+	const title = document.createElement("h3")
+	const username = document.createElement("label")
+	const labelUsername = document.createElement("label")
+	const iUsername = document.createElement("i")
+	const user = document.createElement("input")
+	const password = document.createElement("label")
+	const labelpassword = document.createElement("label")
+	const ipassword = document.createElement("i")
+	const pass = document.createElement("input")
+	const password1 = document.createElement("label")
+	const labelpassword1 = document.createElement("label")
+	const ipassword1 = document.createElement("i")
+	const pass1 = document.createElement("input")
+	const button = document.createElement("button")
+	
+	base.classList.add("container")
+	base.classList.add("content")
+	form.classList.add("form")
+	form.id = "formlogin"
+	title.textContent = "Credentials Form"
+
+	username.classList.add("input")
+	labelUsername.setAttribute("for", "username")
+
+	iUsername.classList.add("fa-solid")
+	iUsername.classList.add("fa-user")
+	iUsername.classList.add("fa-2xs")
+
+	user.id = "username"
+	user.type = "text"
+
+	labelUsername.appendChild(iUsername)
+	username.appendChild(labelUsername)
+	username.appendChild(user)
+
+	password.classList.add("input")
+	labelpassword.setAttribute("for", "password")
+
+	ipassword.classList.add("fa-solid")
+	ipassword.classList.add("fa-lock")
+	ipassword.classList.add("fa-2xs")
+
+	pass.id = "password"
+	pass.type = "password"
+	password.classList.add("input")
+
+	labelpassword1.setAttribute("for", "password1")
+
+	ipassword1.classList.add("fa-solid")
+	ipassword1.classList.add("fa-check")
+	ipassword1.classList.add("fa-2xs")
+
+	pass1.id = "confirmPassword"
+	pass1.type = "password"
+
+	labelUsername.appendChild(iUsername)
+	username.appendChild(labelUsername)
+	username.appendChild(user)
+
+	labelpassword.appendChild(ipassword)
+	password.appendChild(labelpassword)
+	password.appendChild(pass)
+
+	labelpassword1.appendChild(ipassword1)
+	password1.appendChild(labelpassword1)
+	password1.appendChild(pass1)
+
+	button.textContent = "Login"
+	button.id = "proceed"
+
+	user.onchange = (event) => {
+		post("/api/confirm-user", {
+				"method": "POST",
+				"body": JSON.stringify({
+					"username": user.value
+				})
+			}, (r) =>  {
+				if(r.total > 0){
+					password1.style.display = "none"
+					button.textContent = "Login"
+				}else{
+					password1.style.display = "flex"
+					button.textContent = "Register"
+				}
+			}
+		)
+	}
+
+	form.appendChild(title)
+	form.appendChild(username)
+	form.appendChild(password)
+	form.appendChild(password1)
+	form.appendChild(button)
+
+	document.getElementById("poem-contents").innerHTML = ""
+	base.appendChild(form)
+	document.getElementById("poem-contents").appendChild(base)
+
+	button.onclick = () => {
+		post("/api/credentials", {
+			"method": "POST",
+			"body": JSON.stringify({
+				"username": user.value,
+				"password": pass.value,	
+				"password1": pass1.value
+			})
+		}, (response) => {
+			setCookie("userID", response.ID)
+			setCookie("username", response.username)
+			setCookie("credentials", JSON.stringify({
+				"username": response.username,
+				"userID": response.ID
+			}))
+			document.getElementById("login").remove()
+			document.getElementById("loginNav").remove()
 		})
-	}).then(response => {
-		return response.json()
-	}).then(response => {
-		setCookie("userID", response.ID)
-		setCookie("username", response.username)
-		alert("Logged in")
-	}).catch(error => {
-		alert(`There's an error encountered: ${error}`)
-	})
+	}
+
+}
+
+if(getCookie("credentials") != ""){
+	document.getElementById("login").textContent = "Publish"
+	document.getElementById("loginNav").textContent = "Publish"
 }
